b1e3a466b3fe17d7a90763561660c379cfb073ae
rafaelsuarez <rafaelaugustosc@gmail.com>
2021-06-07 11:27:33 -0400

    Routing, switching for HomePAge and HatsPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {Route, Switch} from 'react-router-dom';
+
+
+
+const HatsPage = () =>(
+
+    <div> HATS PAGE</div>
+);
 
 function App() {
   return (
       <div>
-        <HomePage/>
+          <Switch>
+              <Route  exact path='/' component={HomePage}/>
+              <Route  path='/hats' component={HatsPage}/>
+          </Switch>
+
+
       </div>
   );
 
