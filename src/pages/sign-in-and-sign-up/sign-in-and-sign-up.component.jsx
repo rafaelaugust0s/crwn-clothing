91315feff7ec84b86a394b154271490a1a344a82
rafaelsuarez <rafaelaugustosc@gmail.com>
2021-06-17 00:19:27 -0400

    sign-up component and creation of user and implementation with firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 
-const SignInAndSignUp =()=> {
+const SignInAndSignUpPage =()=>
 
 
-    return (
-        <div className='sign-in-and-sign-up'><SignIn/>
+     (
+        <div className='sign-in-and-sign-up'>
+            <SignIn/>
+            <SignUp/>
         </div>
 
-    )
-}
+    );
 
-export default SignInAndSignUp
+
+export default SignInAndSignUpPage
